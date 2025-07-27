7d103deb0005bf4215bb0300311cd2d2d834e2e5
F4R105 <kajubufaraji@gmail.com>
2025-07-27 18:35:08 +0300

    minor bug fixes

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -41,22 +41,31 @@
         <section id="available_events">
             <div class="w-full max-w-6xl p-5 mx-auto">
                 <x-section-header title="My events" />
-                <div class="mb-2 flex gap-4">
-                    <a href="" class="flex gap-2 justify-start items-center hover:underline">
-                        <div class="w-[20px] h-[20px] bg-white"></div>
+                <div class="mb-6 flex gap-4">
+                    <a href="?filter=all" class="flex gap-2 justify-start items-center hover:underline">
+                        <div class="w-[20px] h-[20px] flex">
+                            <span class="w-[5px] bg-white"></span>
+                            <span class="w-[5px] bg-green-300"></span>
+                            <span class="w-[5px] bg-red-300"></span>
+                            <span class="w-[5px] bg-gray-300"></span>
+                        </div>
                         <p class="text-sm">All events</p>
                     </a>
-                    <a href="" class="flex gap-2 justify-start items-center hover:underline">
+                    <a href="?filter=initial" class="flex gap-2 justify-start items-center hover:underline">
                         <div class="w-[20px] h-[20px] bg-white"></div>
                         <p class="text-sm">Booking has not started</p>
                     </a>
-                    <a href="" class="flex gap-2 justify-start items-center hover:underline">
+                    <a href="?filter=booking" class="flex gap-2 justify-start items-center hover:underline">
                         <div class="w-[20px] h-[20px] bg-green-300"></div>
                         <p class="text-sm">Booking is open</p>
                     </a>
-                    <a href="" class="flex gap-2 justify-start items-center hover:underline">
+                    <a href="?filter=preparation" class="flex gap-2 justify-start items-center hover:underline">
                         <div class="w-[20px] h-[20px] bg-red-300"></div>
-                        <p class="text-sm">Booking has passed</p>
+                        <p class="text-sm">Booking complete</p>
+                    </a>
+                    <a href="?filter=expired" class="flex gap-2 justify-start items-center hover:underline">
+                        <div class="w-[20px] h-[20px] bg-gray-300"></div>
+                        <p class="text-sm">Past events</p>
                     </a>
                 </div>
                 <x-user-events :filter="$filter" />
--- resources/views/layouts/system-layout.blade.php
+++ resources/views/layouts/system-layout.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} | Event ticketing system</title>
+    <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
     @vite(['../../resources/css/app.css', '../../resources/js/app.js'])
 </head>
 
